394bf711044895f7a06d529c04721a9913883866
Aggrey Osango <hurrycane661@gmail.com>
2023-09-08 00:33:38 +0300

    Combo box added to select gender

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ ctk.set_appearance_mode ("light")
 
 # Global variable
 app_geometry = "600x520"
-c_radius, padding       = 10, 10
+c_radius, padding       = 7, 10
 framex, framey = 590, 510
 widthx,heighty = 170, 35
 
@@ -18,8 +18,9 @@ x1, y1 = 0.5, 0.4
 x2, y2 = 0.5, 0.5
 x3, y3 = 0.5, 0.6
 x4, y4 = 0.5, 0.7
+x5, y5 = 0.5, 0.8
 
-radius_values           = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+radius_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
 # Initializing the app
@@ -37,11 +38,11 @@ user_pwd = ctk.CTkEntry (master = frame, width = widthx, height = heighty, corne
 
 msg_label = ctk.CTkLabel(master = frame, width = widthx, height = heighty, corner_radius = c_radius, textvariable = text_var, bg_color = "yellow")
 
+options = ctk.CTkComboBox (master = frame, width = widthx, height = heighty, corner_radius = c_radius, values = ["male", "Female"])
 
 def log():
     if (user_name.get() == "aggrey") and (user_pwd.get() == "1234"):
-        text_var.set(user_name.get())
-        # text_var.set(user_pwd.get())
+        text_var.set(f"{user_name.get ()}\n{user_pwd.get()}\n{options.get()}")
         print("Success!!")
     else:
         print("Wrong Name or Password")
@@ -55,10 +56,11 @@ user_name.place(relx = x1, rely= y1, anchor = CENTER)
 
 user_pwd.place(relx=x2, rely=y2, anchor = CENTER)
 
-btn_submit.place(relx=x4, rely=y4, anchor = CENTER)
+options.place(relx=x5, rely=y5, anchor = CENTER)
 
 msg_label.place(relx=x3, rely=y3, anchor = CENTER)
 
+btn_submit.place(relx=x4, rely=y4, anchor = CENTER)
 
 
 
